b1228eeeb69cf596951e12879f079d28ae17cd91
Kirin <ktang91@my.bcit.ca>
2022-01-08 20:41:37 -0800

    welcome&register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,26 @@
+import React from "react";
+
+import classes from "./Register.module.css";
+
 function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.backButton}></div>
+      <div className={classes.welcomeText}>
+        Your personalized experience is ready!
+      </div>
+      <div className={classes.check}></div>
+      <div className={classes.circle}></div>
+      <button className={classes.button}></button>
+      <div className={classes.buttonText}>Create Account</div>
+      <div className={classes.signinMsg}>
+        Already Have an account? 
+        <Link to='/'>
+          <span className={classes.signin}>Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,21 @@
-function WelcomePage() {
-  return <div>Welcome Page</div>;
-}
+import classes from "./Welcome.module.css";
 
+import { Link } from "react-router-dom";
+
+function WelcomePage() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.welcomeTitle}>Welcome</div>
+      <div className={classes.welcomeText}>
+        Let’s begin answering a few questions to customize your <b>slumbr</b> experience.
+      </div>
+      <button className={classes.button}></button>
+      <div className={classes.buttonText}>Get Started</div>
+      <div className={classes.signinMsg}>
+          Already Have an account?
+          <Link to='/'><span className={classes.signin}>Sign In</span></Link>
+      </div>
+    </div>
+  );
+}
 export default WelcomePage;
